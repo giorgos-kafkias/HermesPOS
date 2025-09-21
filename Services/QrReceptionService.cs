ffea7c41a888cf068d836e926354aebc0f3b1f8d
giorgos-kafkias <kafkiasgiorgos@gmail.com>
2025-09-21 14:23:52 +0300

    Added placeholders for QR Reception (models, service, VM, view)

---
+++ Services/QrReceptionService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermesPOS.Services
+{
+    public class QrReceptionService
+    {
+        public void ImportFromQr(string qrUrl)
+        {
+            // TODO: εδώ αργότερα θα βάλουμε τον κώδικα για να κατεβάσει το τιμολόγιο από την ΑΑΔΕ
+        }
+    }
+}
---
+++ ViewModels/QrReceptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HermesPOS.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermesPOS.ViewModels
+{
+    public class QrReceptionViewModel
+    {
+        public ObservableCollection<StockReceptionItem> Items { get; set; }
+            = new ObservableCollection<StockReceptionItem>();
+    }
+
+}
